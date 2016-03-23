484f2c404f759f2339a85be9683757d22c97499d
BoB <bob@steinbauerusa.com>
2016-03-22 21:52:05 -0400

    file download (broken)

--- main.js
+++ main.js
@@ -317,3 +317,97 @@ function handleFile(e) {
 }
 
 if(xlf.addEventListener) xlf.addEventListener('change', handleFile, false);
+
+///////////// WRITE /////////////////////
+
+function datenum(v, date1904) {
+	if(date1904) v+=1462;
+	var epoch = Date.parse(v);
+	return (epoch - new Date(Date.UTC(1899, 11, 30))) / (24 * 60 * 60 * 1000);
+}
+
+function sheet_from_array_of_arrays(data, opts) {
+	var ws = {};
+	var range = {s: {c:10000000, r:10000000}, e: {c:0, r:0 }};
+	for(var R = 0; R != data.length; ++R) {
+		for(var C = 0; C != data[R].length; ++C) {
+			if(range.s.r > R) range.s.r = R;
+			if(range.s.c > C) range.s.c = C;
+			if(range.e.r < R) range.e.r = R;
+			if(range.e.c < C) range.e.c = C;
+			var cell = {v: data[R][C] };
+			if(cell.v == null) continue;
+			var cell_ref = XLSX.utils.encode_cell({c:C,r:R});
+
+			if(typeof cell.v === 'number') cell.t = 'n';
+			else if(typeof cell.v === 'boolean') cell.t = 'b';
+			else if(cell.v instanceof Date) {
+				cell.t = 'n'; cell.z = XLSX.SSF._table[14];
+				cell.v = datenum(cell.v);
+			}
+			else cell.t = 's';
+
+			ws[cell_ref] = cell;
+		}
+	}
+	if(range.s.c < 10000000) ws['!ref'] = XLSX.utils.encode_range(range);
+	return ws;
+}
+
+function Workbook() {
+	if(!(this instanceof Workbook)) return new Workbook();
+	this.SheetNames = [];
+	this.Sheets = {};
+}
+
+function getMatrix(scriptLines) {
+  var out = [];
+  var header = ['File Name', 'Segments...'];
+  // for (var i=0; i < 19; i++) {
+  //   header.push('');
+  // }
+  out.push(header);
+  for (var lineIdx=0; lineIdx < scriptLinesList.length; lineIdx++) {
+    var key = scriptLinesList[lineIdx];
+    var line = scriptLines[key];
+    var row = [line.filename || 'ERROR'];
+    for (var segmentIdx=0; segmentIdx<20; segmentIdx++) {
+      if (line.segments.length >= (segmentIdx+1)) {
+        row.push(line.segments[segmentIdx]);
+      } else {
+        continue;
+        // row.push('');
+      }
+    }
+    out.push(row);
+  }
+  return out;
+}
+
+function download() {
+  var SHEET_NAME = 'merged';
+
+  if (scriptLinesList.length <= 0) {
+    alert('wat');
+    return
+  }
+
+  var data = getMatrix(scriptLines);
+  var wb = new Workbook();
+  var ws = sheet_from_array_of_arrays(data);
+  wb.SheetNames.push(SHEET_NAME);
+  wb.Sheets[SHEET_NAME] = ws;
+  console.log(wb);
+
+
+  function s2ab(s) {
+    var buf = new ArrayBuffer(s.length);
+    var view = new Uint8Array(buf);
+    for (var i=0; i!=s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
+    return buf;
+  }
+  var wbout = XLSX.write(wb, {bookType:'xlsx', bookSST:true, type: 'binary'});
+  saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), "merged-scripts.xlsx")
+  // var wbout = XLS.write(wb, {bookType:'xls', bookSST:true, type: 'binary'});
+  // saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), "merged-scripts.xls")
+}
